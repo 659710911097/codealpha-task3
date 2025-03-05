d42588786e7bd43ea25e22b5d5055b73db269d86
659710911097 <laghariaamna56@gmail.com>
2025-03-05 14:49:27 +0500

    Add files via upload

---
+++ Codealpha task3.cpp
@@ -0,0 +1,96 @@
+
+#include <iostream>
+using namespace std;
+
+// Function to print the Sudoku board
+void printBoard(int board[9][9]) {
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            cout << board[i][j] << " ";
+            if ((j + 1) % 3 == 0 && j < 8) {
+                cout << "| ";
+            }
+        }
+        cout << endl;
+        if ((i + 1) % 3 == 0 && i < 8) {
+            cout << "---------------------" << endl;
+        }
+    }
+}
+
+// Function to check if a number can be placed at a given position
+bool isValid(int board[9][9], int row, int col, int num) {
+    // Check the row
+    for (int i = 0; i < 9; i++) {
+        if (board[row][i] == num) {
+            return false;
+        }
+    }
+
+    // Check the column
+    for (int i = 0; i < 9; i++) {
+        if (board[i][col] == num) {
+            return false;
+        }
+    }
+
+    // Check the box
+    int boxRow = row - row % 3;
+    int boxCol = col - col % 3;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (board[boxRow + i][boxCol + j] == num) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+// Function to solve the Sudoku puzzle using backtracking
+bool solveSudoku(int board[9][9]) {
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            if (board[i][j] == 0) {
+                for (int num = 1; num <= 9; num++) {
+                    if (isValid(board, i, j, num)) {
+                        board[i][j] = num;
+                        if (solveSudoku(board)) {
+                            return true;
+                        }
+                        board[i][j] = 0;
+                    }
+                }
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+int main() {
+    int board[9][9] = {
+        {5, 3, 0, 0, 7, 0, 0, 0, 0},
+        {6, 0, 0, 1, 9, 5, 0, 0, 0},
+        {0, 9, 8, 0, 0, 0, 0, 6, 0},
+        {8, 0, 0, 0, 6, 0, 0, 0, 3},
+        {4, 0, 0, 8, 0, 3, 0, 0, 1},
+        {7, 0, 0, 0, 2, 0, 0, 0, 6},
+        {0, 6, 0, 0, 0, 0, 2, 8, 0},
+        {0, 0, 0, 4, 1, 9, 0, 0, 5},
+        {0, 0, 0, 0, 8, 0, 0, 7, 9}
+    };
+
+    cout << "Original Sudoku board:" << endl;
+    printBoard(board);
+
+    if (solveSudoku(board)) {
+        cout << "Solved Sudoku board:" << endl;
+        printBoard(board);
+    } else {
+        cout << "No solution exists for this Sudoku puzzle." << endl;
+    }
+
+    return 0;
+}
